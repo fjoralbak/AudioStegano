ca551d104d497e12f0ef399086b64ad8fc6c45cb
fjoralbak <76589053+fjoralbak@users.noreply.github.com>
2021-05-19 23:47:42 +0200

    Fshehja e informates ne audio file (.wav) dhe leximi i audio file te ri pas fshehjes se informates
    Fillimisht eshte bere leximi i audio file (.wav) para se te fshihet informata pastaj eshte bere fshehja e informates ne te dhe si perfundim leximi i audio-s pas fshehjes se informates ku ne message box paraqitet mesazhi i fshehur

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,16 +38,18 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(64, 55);
+            this.textBox1.Location = new System.Drawing.Point(48, 45);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(423, 22);
+            this.textBox1.Size = new System.Drawing.Size(318, 20);
             this.textBox1.TabIndex = 0;
             // 
             // btnChooseFile
             // 
-            this.btnChooseFile.Location = new System.Drawing.Point(493, 50);
+            this.btnChooseFile.Location = new System.Drawing.Point(370, 41);
+            this.btnChooseFile.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnChooseFile.Name = "btnChooseFile";
-            this.btnChooseFile.Size = new System.Drawing.Size(100, 32);
+            this.btnChooseFile.Size = new System.Drawing.Size(75, 26);
             this.btnChooseFile.TabIndex = 1;
             this.btnChooseFile.Text = "Choose File";
             this.btnChooseFile.UseVisualStyleBackColor = true;
@@ -55,9 +57,10 @@
             // 
             // btnPlay
             // 
-            this.btnPlay.Location = new System.Drawing.Point(64, 102);
+            this.btnPlay.Location = new System.Drawing.Point(48, 83);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(96, 44);
+            this.btnPlay.Size = new System.Drawing.Size(72, 36);
             this.btnPlay.TabIndex = 2;
             this.btnPlay.Text = "Play";
             this.btnPlay.UseVisualStyleBackColor = true;
@@ -65,9 +68,10 @@
             // 
             // btnStop
             // 
-            this.btnStop.Location = new System.Drawing.Point(390, 102);
+            this.btnStop.Location = new System.Drawing.Point(292, 83);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(97, 44);
+            this.btnStop.Size = new System.Drawing.Size(73, 36);
             this.btnStop.TabIndex = 3;
             this.btnStop.Text = "Stop";
             this.btnStop.UseVisualStyleBackColor = true;
@@ -75,18 +79,21 @@
             // 
             // btnHide
             // 
-            this.btnHide.Location = new System.Drawing.Point(64, 152);
+            this.btnHide.Location = new System.Drawing.Point(48, 124);
+            this.btnHide.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnHide.Name = "btnHide";
-            this.btnHide.Size = new System.Drawing.Size(96, 38);
+            this.btnHide.Size = new System.Drawing.Size(72, 31);
             this.btnHide.TabIndex = 4;
             this.btnHide.Text = "Hide";
             this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
             // 
             // btnStegano
             // 
-            this.btnStegano.Location = new System.Drawing.Point(390, 152);
+            this.btnStegano.Location = new System.Drawing.Point(292, 124);
+            this.btnStegano.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnStegano.Name = "btnStegano";
-            this.btnStegano.Size = new System.Drawing.Size(97, 38);
+            this.btnStegano.Size = new System.Drawing.Size(73, 31);
             this.btnStegano.TabIndex = 5;
             this.btnStegano.Text = "Stegano";
             this.btnStegano.UseVisualStyleBackColor = true;
@@ -94,15 +101,16 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(635, 273);
+            this.ClientSize = new System.Drawing.Size(476, 222);
             this.Controls.Add(this.btnStegano);
             this.Controls.Add(this.btnHide);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnPlay);
             this.Controls.Add(this.btnChooseFile);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- Form1.cs
+++ Form1.cs
@@ -15,9 +15,51 @@ namespace Steganography_Wav
     public partial class Form1 : Form
     {
         SoundPlayer player;
+
+        private System.Byte[] bufferInternal_uint8 = null;
+        private System.Int16[] bufferInternal_int16 = null;
+        private byte[] data = null;
+
+        private System.UInt32 _numberOfSamples;
+        private byte[] chunk_id = new Byte[4];       
+        private System.UInt32 chunk_size;
+        private byte[] format = new Byte[4];         
+        private byte[] fmtchunk_id = new Byte[4];    
+        private System.UInt32 fmtchunk_size;
+        private System.UInt16 audio_format;
+        private System.UInt16 num_channels;
+        private System.UInt32 sample_rate;
+        private System.UInt32 byte_rate;
+        private System.UInt16 block_align;
+        private System.UInt16 bps;                     
+        private byte[] datachunk_id = new Byte[4];    
+        private System.UInt32 datachunk_size;
+
+        private String globalFilePath = "";
+
+        byte[] byteText = Encoding.UTF8.GetBytes("FIEK");
+
+        public enum NUM_CHANNELS
+        {
+            NOT_DEFINED = 0,
+            ONE = 1,
+            TWO = 2
+        };
+        public enum BITS_PER_SAMPLE
+        {
+            NOT_DEFINED = 0,
+            BPS_8_BITS = 8,
+            BPS_16_BITS = 16
+        };
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnHide.Enabled = false;
+            btnStegano.Enabled = false;
         }
 
         private void btnChooseFile_Click(object sender, EventArgs e)
@@ -29,30 +71,6 @@ namespace Steganography_Wav
             }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            btnHide.Enabled = false;
-            btnStegano.Enabled = false;
-        }
-
-        private void btnPlay_Click(object sender, EventArgs e)
-        {
-
-            try
-            {
-                player = new SoundPlayer();
-                player.SoundLocation = textBox1.Text;
-                player.Load();
-                player.Play();
-                btnPlay.Enabled = false;
-                btnStop.Enabled = true;
-
-            }
-            catch (Win32Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
         private void btnStop_Click(object sender, EventArgs e)
         {
             player.Stop();
@@ -60,10 +78,251 @@ namespace Steganography_Wav
             btnStop.Enabled = false;
         }
 
-        private void btnStegano_Click(object sender, EventArgs e)
+
+        private void btnPlay_Click(object sender, EventArgs e)
         {
 
-            byte[] bytes = File.ReadAllBytes(textBox1.Text);
+            try
+            {
+                player = new SoundPlayer();
+
+                player.SoundLocation = textBox1.Text;
+                globalFilePath = textBox1.Text;
+                var filePath = textBox1.Text;
+
+                using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+                {
+                    chunk_id = reader.ReadBytes(4);   
+                    chunk_size = reader.ReadUInt32();
+                    format = reader.ReadBytes(4);   
+                    fmtchunk_id = reader.ReadBytes(4);   
+                    fmtchunk_size = reader.ReadUInt32();
+                    audio_format = reader.ReadUInt16();
+                    num_channels = reader.ReadUInt16();
+                    sample_rate = reader.ReadUInt32();
+                    byte_rate = reader.ReadUInt32();
+                    block_align = reader.ReadUInt16();
+                    bps = reader.ReadUInt16();    
+                    datachunk_id = reader.ReadBytes(4);  
+                    datachunk_size = reader.ReadUInt32();
+
+
+                    if (System.Text.Encoding.ASCII.GetString(chunk_id) != "RIFF"|| System.Text.Encoding.ASCII.GetString(format) != "WAVE")
+                    {
+                        throw new ApplicationException("ERROR: File " + filePath + " is not a WAV file");
+                    }
+                    if (audio_format != 1)
+                    {
+                        throw new ApplicationException("ERROR: File " + filePath + " the API only supports PCM format in WAV.");
+                    }
+
+
+                    switch ((BITS_PER_SAMPLE)bps)
+                    {
+
+                        case BITS_PER_SAMPLE.BPS_8_BITS:
+
+                            bufferInternal_uint8 = reader.ReadBytes((int)datachunk_size);
+                            data = bufferInternal_uint8;
+                            _numberOfSamples = datachunk_size / num_channels;
+
+                            break;
+
+
+                        case BITS_PER_SAMPLE.BPS_16_BITS:
+
+                            int num_int16 = (int)(datachunk_size / sizeof(System.Int16));
+
+                            bufferInternal_int16 = new System.Int16[num_int16];
+                            byte[] two_byte_buf_to_int16;
+
+                            for (int i = 0; i < num_int16; i++)
+                            {
+                                two_byte_buf_to_int16 = reader.ReadBytes(2);
+                                bufferInternal_int16[i] = BitConverter.ToInt16(two_byte_buf_to_int16, 0);
+                            }
+
+                            _numberOfSamples = (datachunk_size / 2) / num_channels;
+                            break;
+
+                        default:
+                            throw new ApplicationException("ERROR: Incorret bits per sample in file " + filePath);
+                    }
+                }
+                player.Load();
+                player.Play();
+
+                btnPlay.Enabled = false;
+                btnStop.Enabled = true;
+                btnStegano.Enabled = true;
+                btnHide.Enabled = true;
+
+            }
+            catch (Win32Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnStegano_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            try
+            {
+                player = new SoundPlayer();
+
+                player.SoundLocation = textBox1.Text;
+                var filePath = textBox1.Text;
+
+
+                using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+                {
+
+                    var new_chunk_id = reader.ReadBytes(4);
+                    var new_chunk_size = reader.ReadUInt32();
+                    var new_format = reader.ReadBytes(4);
+                    var new_fmtchunk_id = reader.ReadBytes(4);
+                    var new_fmtchunk_size = reader.ReadUInt32();
+                    var new_audio_format = reader.ReadUInt16();
+                    var new_num_channels = reader.ReadUInt16();
+                    var new_sample_rate = reader.ReadUInt32();
+                    var new_byte_rate = reader.ReadUInt32();
+                    var new_block_align = reader.ReadUInt16();
+                    var new_bps = reader.ReadUInt16();
+                    var new_datachunk_id = reader.ReadBytes(4);
+                    var new_datachunk_size = reader.ReadUInt32();
+
+
+                    if (System.Text.Encoding.ASCII.GetString(chunk_id) != "RIFF"|| System.Text.Encoding.ASCII.GetString(format) != "WAVE")
+                    {
+                        throw new ApplicationException("ERROR: File " + filePath + " is not a WAV file");
+                    }
+                    if (audio_format != 1)
+                    {
+                        throw new ApplicationException("ERROR: File " + filePath + " the API only supports PCM format in WAV.");
+                    }
+
+                    int num_int16 = (int)(datachunk_size / sizeof(System.Int16));
+
+                    var new_bufferInternal_int16 = new System.Double[num_int16];
+                    byte[] two_byte_buf_to_int16;
+
+                    for (int i = 0; i < num_int16; i++)
+                    {
+                        double byteFromText = 0.0;
+
+                        if (i < byteText.Length)
+                        {
+                            two_byte_buf_to_int16 = reader.ReadBytes(8);
+                            new_bufferInternal_int16[i] = BitConverter.ToDouble(two_byte_buf_to_int16, 0);
+
+                            var hiddenValue = new_bufferInternal_int16[i] - bufferInternal_int16[i];
+                            var hiddenValueToByte = Math.Round(hiddenValue, 3) * 1000.00;
+                            var word = (char)hiddenValueToByte;
+
+                            sb.Append(word);
+                        }
+                        else
+                        {
+                            two_byte_buf_to_int16 = reader.ReadBytes(2);
+                            new_bufferInternal_int16[i] = BitConverter.ToInt16(two_byte_buf_to_int16, 0);
+                        }
+                    }
+
+                    MessageBox.Show("Teksti i lexuar eshte: " + sb.ToString());
+                }
+            }
+            catch (Win32Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            var filePath = "";
+            var pathAsArray = globalFilePath.Split('\\');
+            var index = 0;
+
+            foreach (var pathItem in pathAsArray)
+            {
+                if (index < pathAsArray.Length - 1)
+                {
+                    filePath += pathItem + "\\";
+                }
+                else if (index == pathAsArray.Length - 1)
+                {
+                    filePath += "\\steganoFile.wav";
+                }
+                index++;
+            }
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+            {
+                writer.Write(chunk_id);
+                writer.Write(chunk_size);
+                writer.Write(format);
+                writer.Write(fmtchunk_id);
+                writer.Write(fmtchunk_size);
+                writer.Write(audio_format);
+                writer.Write(num_channels);
+                writer.Write(sample_rate);
+                writer.Write(byte_rate);
+                writer.Write(block_align);
+                writer.Write(bps);
+                writer.Write(datachunk_id);
+                writer.Write(datachunk_size);
+
+                switch ((BITS_PER_SAMPLE)bps)
+                {
+                    case BITS_PER_SAMPLE.BPS_8_BITS:
+
+                        if (bufferInternal_uint8 == null)
+                        {
+                            throw new ApplicationException("ERROR: Data buffer uint8 is NULL!");
+                        }
+
+                        writer.Write(bufferInternal_uint8);
+
+                        break;
+
+                    case BITS_PER_SAMPLE.BPS_16_BITS:
+
+                        if (bufferInternal_int16 == null)
+                        {
+                            throw new ApplicationException("ERROR: Data buffer int16 is NULL!");
+                        }
+
+                        int num_int16 = (int)(datachunk_size / sizeof(System.Int16));
+                        byte[] two_bytes_buf_from_int16;
+
+                        for (int i = 0; i < num_int16; i++)
+                        {
+                            double byteFromText = 0.0;
+
+                            if (i < byteText.Length)
+                            {
+                                byteFromText = (double)((int)byteText[i]) / 1000.00;
+                                var hiddenValue = bufferInternal_int16[i] + byteFromText;
+                                two_bytes_buf_from_int16 = BitConverter.GetBytes(hiddenValue);
+                            }
+                            else
+                            {
+                                two_bytes_buf_from_int16 = BitConverter.GetBytes(bufferInternal_int16[i]);
+                            }
+                            writer.Write(two_bytes_buf_from_int16);
+                        }
+                        break;
+
+                    default:
+
+                        throw new ApplicationException("ERROR: Incorret bits per sample to write file " + filePath);
+                }
+            }
         }
     }
 }
+
+
+
