51654ecc2c7b896728b8b5f7e3406a1f4f2ee846
Era Tahiri <66219451+Era-T@users.noreply.github.com>
2021-05-19 22:50:57 +0200

    Krijimi i formes me butona

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Steganography_Wav
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.btnStegano = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(423, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Location = new System.Drawing.Point(493, 50);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(100, 32);
+            this.btnChooseFile.TabIndex = 1;
+            this.btnChooseFile.Text = "Choose File";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(64, 102);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(96, 44);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(390, 102);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(97, 44);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(64, 152);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(96, 38);
+            this.btnHide.TabIndex = 4;
+            this.btnHide.Text = "Hide";
+            this.btnHide.UseVisualStyleBackColor = true;
+            // 
+            // btnStegano
+            // 
+            this.btnStegano.Location = new System.Drawing.Point(390, 152);
+            this.btnStegano.Name = "btnStegano";
+            this.btnStegano.Size = new System.Drawing.Size(97, 38);
+            this.btnStegano.TabIndex = 5;
+            this.btnStegano.Text = "Stegano";
+            this.btnStegano.UseVisualStyleBackColor = true;
+            this.btnStegano.Click += new System.EventHandler(this.btnStegano_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 273);
+            this.Controls.Add(this.btnStegano);
+            this.Controls.Add(this.btnHide);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnChooseFile);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnChooseFile;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.Button btnStegano;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Media;
+
+namespace Steganography_Wav
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer player;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnHide.Enabled = false;
+            btnStegano.Enabled = false;
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                player = new SoundPlayer();
+                player.SoundLocation = textBox1.Text;
+                player.Load();
+                player.Play();
+                btnPlay.Enabled = false;
+                btnStop.Enabled = true;
+
+            }
+            catch (Win32Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+            btnPlay.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void btnStegano_Click(object sender, EventArgs e)
+        {
+
+            byte[] bytes = File.ReadAllBytes(textBox1.Text);
+        }
+    }
+}
